96cfcb96a6252b51d652783279274bf7479c2303
Matt Levy <milevy1@gmail.com>
2019-02-12 10:59:18 -0700

    Added required files and changed changed methods to class methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,4 +1,8 @@
 require 'csv'
+require './lib/game'
+require './lib/game_team'
+require './lib/team'
+
 class StatTracker
   def initialize(games, teams, game_teams)
     @games = games
@@ -13,7 +17,7 @@ class StatTracker
     StatTracker.new(games, teams, game_teams)
   end
 
-  def create_game_objects(file_location)
+  def self.create_game_objects(file_location)
     games = []
     CSV.foreach(file_location, headers: true, header_converters:  :symbol) do |row|
       games << Game.new(row)
@@ -21,7 +25,7 @@ class StatTracker
     return games
   end
 
-  def create_teams_objects(team_file)
+  def self.create_teams_objects(team_file)
     teams = []
     CSV.foreach(team_file, headers: true, header_converters:  :symbol) do |row|
       teams << Team.new(row)
@@ -29,7 +33,7 @@ class StatTracker
     return teams
   end
 
-  def create_game_teams_objects(game_teams_file)
+  def self.create_game_teams_objects(game_teams_file)
     game_teams = []
     CSV.foreach(game_team_file, headers: true, header_converters:  :symbol) do |row|
       game_teams << GameTeam.new(row)
