6eb1b61f0bc5f93b0115be9b625ddd917357e56a
Matt Levy <milevy1@gmail.com>
2019-02-12 11:19:28 -0700

    Added unit conversions for game_team columns

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -16,20 +16,25 @@ class GameTeam
               :takeaways
 
   def initialize(row)
-    @game_id = row[:game_id]
-    @team_id = row[:team_id]
+    @game_id = row[:game_id].to_i
+    @team_id = row[:team_id].to_i
     @HoA = row[:HoA]
-    @won = row[:won]
+    @won = string_to_boolean(row[:won]) # Converts string to boolean
     @settled_in = row[:settled_in]
     @head_coach = row[:head_coach]
-    @goals = row[:goals]
-    @shots = row[:shots]
-    @hits = row[:hits]
-    @pim = row[:pim]
-    @powerPlayOpportunities = row[:powerPlayOpportunities]
-    @powerPlayGoals = row[:powerPlayGoals]
-    @faceOffWinPercentage = row[:faceOffWinPercentage]
-    @giveaways = row[:giveaways]
-    @takeaways = row[:takeaways]
+    @goals = row[:goals].to_i
+    @shots = row[:shots].to_i
+    @hits = row[:hits].to_i
+    @pim = row[:pim].to_i
+    @powerPlayOpportunities = row[:powerPlayOpportunities].to_i
+    @powerPlayGoals = row[:powerPlayGoals].to_i
+    @faceOffWinPercentage = row[:faceOffWinPercentage].to_f
+    @giveaways = row[:giveaways].to_i
+    @takeaways = row[:takeaways].to_i
   end
+
+  def string_to_boolean(string)
+    string == "TRUE"
+  end
+
 end
