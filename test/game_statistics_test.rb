1f3107a978cc077011f7600ccc579e42303af7b3
Mills Provosty <millsprovosty@gmail.com>
2019-02-12 15:12:26 -0700

    tested highest total score

--- test/game_statistics_test.rb
+++ test/game_statistics_test.rb
@@ -1,6 +1,28 @@
 require './test/test_helper'
 require './lib/stat_tracker'
+require './lib/game_statistics'
 
 class GameStatisticsTest < Minitest::Test
-  
+  include GameStatistics
+
+  def setup
+
+    game_path = './data/game_dummy.csv'
+    team_path = './data/team_info.csv'
+    game_teams_path = './data/game_teams_stats_dummy.csv'
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    @stat_tracker = StatTracker.from_csv(locations)
+
+  end
+
+
+  def test_highest_total_score
+
+    assert_equal 7, @stat_tracker.highest_total_score
+  end
+
 end
