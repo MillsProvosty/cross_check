af9700bbc895e3f704804a3caed7716532e9e9a5
Stella Mainar <mainars09@alumni.hanover.edu>
2019-02-13 16:59:59 -0700

    Add method for total goals by team

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -4,4 +4,21 @@ module LeagueStatistics
     @teams.count
   end
 
+  def total_goals_by_team
+    total_goals = {}
+    @games.each do |game|
+      if total_goals[game.away_team_id].nil?
+        total_goals[game.away_team_id] = game.away_goals
+      else
+        total_goals[game.away_team_id] += game.away_goals
+      end
+      if total_goals[game.home_team_id].nil?
+        total_goals[game.home_team_id] = game.home_goals
+      else
+        total_goals[game.home_team_id] += game.home_goals
+      end
+    end
+    return total_goals
+  end
+
 end
