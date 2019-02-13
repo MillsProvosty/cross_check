b26651d4d57ab978df0f093d1745fa3e248f1f87
Matt Levy <milevy1@gmail.com>
2019-02-12 22:44:52 -0700

    Rearranged hash in test average goals by season

--- test/game_statistics_test.rb
+++ test/game_statistics_test.rb
@@ -55,8 +55,8 @@ class GameStatisticsTest < Minitest::Test
 
   def test_average_goals_by_season
     expected = { 20122013 => 4.8,
+                 20142015 => 3.0,
                  20152016 => 4.0,
-                 20142015 => 3.0, 
                  20172018 => 6.0 }
 
     assert_equal expected, @stat_tracker.average_goals_by_season
