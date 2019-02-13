05e34864349a306bfc157a0d9912195e3e176662
Stella Mainar <mainars09@alumni.hanover.edu>
2019-02-13 16:42:45 -0700

    Add total goals by team test

--- test/league_statistics_test.rb
+++ test/league_statistics_test.rb
@@ -22,4 +22,28 @@ class LeagueStatisticsTest < Minitest::Test
   def test_count_of_teams
     assert_equal 33, @stat_tracker.count_of_teams
   end
+
+  def test_total_number_of_goals_by_team
+    expected = {1 => 1,
+                3 => 5,
+                5 => 3,
+                6 => 10,
+                7 => 2,
+                12 => 1,
+                15 => 3,
+                16 => 2,
+                19 => 4,
+                22 => 1,
+                23 => 4,
+                27 => 3,
+                28 => 3,
+                30 => 3}
+
+    assert_equal expected, @stat_tracker.total_goals_by_team
+  end
+
+  # Step 1: Create hash
+  # Step 2: Each game has Away Goals & Home Goals
+  # Step 3:
+
 end
