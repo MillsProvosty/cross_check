9212f03aea87185bab27f6de21331fac0ddda1ae
Stella Mainar <mainars09@alumni.hanover.edu>
2019-02-13 17:37:20 -0700

    Add test for average goals by team

--- test/league_statistics_test.rb
+++ test/league_statistics_test.rb
@@ -60,4 +60,22 @@ class LeagueStatisticsTest < Minitest::Test
     assert_equal expected, @stat_tracker.games_played_by_team
   end
 
+  def test_average_goals_by_team
+    expected = {1 => 1.0,
+                3 => 1.67,
+                5 => 3.0,
+                6 => 3.33,
+                7 => 2.0,
+                12 => 1.0,
+                15 => 3.0,
+                16 => 2.0,
+                19 => 4.0,
+                22 => 1.0,
+                23 => 4.0,
+                27 => 3.0,
+                28 => 1.5,
+                30 => 1.5}
+    assert_equal expected, @stat_tracker.average_goals_by_team
+  end
+
 end
