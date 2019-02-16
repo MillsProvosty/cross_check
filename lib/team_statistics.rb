5d2f6036d7d2f283c00d41ae5098c020038f58ca
Mills Provosty <millsprovosty@gmail.com>
2019-02-16 14:40:18 -0700

    added best season

--- lib/team_statistics.rb
+++ lib/team_statistics.rb
@@ -1,10 +1,13 @@
 module TeamStatistics
 
   def best_season(team_id)
-    #Season with the highest win percentage for a team.
-
-    
     #select each game by team_id
+    games_grouped_by_id = @games.group_by do |game|
+      game.away_team_id || game.home_team_id
+    end
+    
+
+
     #group by season
     #calcuate wins/total_games
     #.max
