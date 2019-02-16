e6f68103f6939a3ae21af56ce4f414e0de4a9a84
Mills Provosty <millsprovosty@gmail.com>
2019-02-16 14:30:33 -0700

    Initial commit

---
+++ lib/team_statistics.rb
@@ -0,0 +1,14 @@
+module TeamStatistics
+
+  def best_season(team_id)
+    #Season with the highest win percentage for a team.
+
+    
+    #select each game by team_id
+    #group by season
+    #calcuate wins/total_games
+    #.max
+
+  end
+
+end
