39f66afacc5637fe388eedbbe6fcba27596a76d8
Mills Provosty <millsprovosty@gmail.com>
2019-02-16 14:59:00 -0700

    Added calculating wins by games

--- lib/team_statistics.rb
+++ lib/team_statistics.rb
@@ -2,13 +2,31 @@ module TeamStatistics
 
   def best_season(team_id)
     #select each game by team_id
-    games_grouped_by_id = @games.group_by do |game|
-      game.away_team_id || game.home_team_id
+    games_by_team_id = @games.find_all do |game|
+      game.away_team_id == team_id || game.home_team_id == team_id
     end
-    
-
 
     #group by season
+    games_by_season = games_by_team_id.group_by do |game|
+      game.season
+    end
+
+    #calcuate wins/total_games
+    total_wins_by_season = {}
+    games_by_season.each do |season_id, games|
+      total_wins_by_season[season_id] = 0
+      games.each do |game|
+        if game.home_team_id == team_id && game.outcome.include?("home win")
+          total_wins_by_season[season_id] += 1
+        elsif game.away_team_id == team_id &&
+          game.outcome.include?("away win")
+          total_wins_by_season[season_id] += 1
+        end
+      end
+    end
+    binding.pry
+
+
     #calcuate wins/total_games
     #.max
 
