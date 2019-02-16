540e3cd096aea61dc49ba8b1737763408fc7b850
Stella Mainar <mainars09@alumni.hanover.edu>
2019-02-16 16:18:39 -0700

    Initial commit

---
+++ test/seasons_statistics_test.rb
@@ -0,0 +1,19 @@
+require './test/test_helper'
+require './lib/stat_tracker'
+require './lib/season_statistics'
+
+
+class SeasonStatisticsTest < Minitest::Test
+  def setup
+    game_path = './data/game_dummy.csv'
+    team_path = './data/team_info.csv'
+    game_teams_path = './data/game_teams_stats_dummy.csv'
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+
+end
