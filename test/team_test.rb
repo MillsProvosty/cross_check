a522d7f817b7ed15d7729ed3f602a41af3c28863
Mills Provosty <millsprovosty@gmail.com>
2019-02-15 22:07:10 -0700

    Working on game by teams method

--- test/team_test.rb
+++ test/team_test.rb
@@ -5,28 +5,35 @@ require './lib/game_statistics'
 
 class TeamTest < Minitest::Test
   def setup
-    @team = Team.new
+    @team = Team.new(row[])
 
     assert_equal Team, @team
   end
 
-  def test_best_season
-    #select each game by team_id
-    #group by season
-    #calcuate wins/total_games
-    #.max
+#Average number of goals scored in a game across all seasons
+# Season with the highest win percentage for a team.
+#select each game by team_id
+#group by season
+#calcuate wins/total_games
+#.max
 
-    assert_equal
+
+  def test_game_by_teams
+    expected = [2012030221, 2012030222]
+
+  assert_equal [expected], @teams.game_by_teams(3)
   end
 
-  def test_get_team_id  
-  end
-
-  def test_group_by_season
-  end
-
-  def test_calculate_percentage
-  end
-
+  # def test_best_season
+  #
+  #   assert_equal 20132014, @team.best_season
+  # end
+  #
+  # def test_group_by_season
+  # end
+  #
+  # def test_calculate_percentage
+  # end
+  #
 
 end
