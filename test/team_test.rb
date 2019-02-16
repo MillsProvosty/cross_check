1769d58ca8ef5e0a13addcb2186d5d3a840b7e78
Mills Provosty <millsprovosty@gmail.com>
2019-02-15 17:42:09 -0700

    Initial test setup

--- test/team_test.rb
+++ test/team_test.rb
@@ -1,8 +1,32 @@
 require './test/test_helper'
 require './lib/stat_tracker'
 require './lib/game_statistics'
-require './lib.team'
+
 
 class TeamTest < Minitest::Test
-  
+  def setup
+    @team = Team.new
+
+    assert_equal Team, @team
+  end
+
+  def test_best_season
+    #select each game by team_id
+    #group by season
+    #calcuate wins/total_games
+    #.max
+
+    assert_equal
+  end
+
+  def test_get_team_id  
+  end
+
+  def test_group_by_season
+  end
+
+  def test_calculate_percentage
+  end
+
+
 end
