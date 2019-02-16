b1843e1aeb4ab7943c7209654a0c1a1f41d92b5c
Mills Provosty <millsprovosty@gmail.com>
2019-02-15 22:07:40 -0700

    adding game by teams method

--- lib/team.rb
+++ lib/team.rb
@@ -13,6 +13,23 @@ class Team
     @teamname = row[:teamname]
     @abbreviation = row[:abbreviation]
     @link = row[:link]
+
   end
+
+  def game_by_teams(team_id)
+    game_array = []
+    @games.each do |game|
+      game_array << @games.team_id
+    end
+  end
+
+  def count_wins_by_team_id
+    win_count = game_array.each do |game|
+      game.wins
+    if @stat_tracker.wins == true
+      hash += 1
+  end
+
+
 
 end
