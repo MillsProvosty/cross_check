1776402981c2b8e9a0f73ceec18b78c9add038b9
Mills Provosty <millsprovosty@gmail.com>
2019-02-17 16:33:45 -0700

    Added fewest goals scored test

--- test/team_statistics_test.rb
+++ test/team_statistics_test.rb
@@ -30,4 +30,9 @@ class TeamTest < Minitest::Test
 
     assert_equal 5, @stat_tracker.most_goals_scored(6)
   end
+
+  def test_fewest_goals_scored
+
+    assert_equal 5, @stat_tracker.fewest_goals_scored(6)
+  end
 end
