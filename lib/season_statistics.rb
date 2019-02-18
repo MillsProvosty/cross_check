91d8b64ddf3ae7310284b9260632094bcc5cc9a2
Matt Levy <milevy1@gmail.com>
2019-02-17 21:07:30 -0700

    Added least_hits

--- lib/season_statistics.rb
+++ lib/season_statistics.rb
@@ -34,6 +34,34 @@ module SeasonStatistics
     team_object.teamname
   end
 
+  def least_hits(season_id)
+    # 1) Find all games with the season id
+      all_games_with_season_id = find_games_by_season_id(season_id)
+
+    # 2) Create hash, key = team_id and value = number_of_hits
+      team_id_with_number_of_hits = {}
+      all_games_with_season_id.each do |game|
+        if team_id_with_number_of_hits[game.team_id].nil?
+          team_id_with_number_of_hits[game.team_id] = game.hits
+
+        else
+          team_id_with_number_of_hits[game.team_id] += game.hits
+        end
+      end
+
+    # 3) Find the max
+      max_team_id = team_id_with_number_of_hits.min_by do |team_id, hits|
+        hits
+      end.first
+
+      # 4) Convert team_id to teamname
+      team_object =  @teams.find do |team|
+        team.team_id == max_team_id
+      end
+
+      team_object.teamname
+  end
+
   def power_play_goal_percentage(season_id)
     # 1) Find all games with the season id
     all_games_with_season_id = find_games_by_season_id(season_id)
