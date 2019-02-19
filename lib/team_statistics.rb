a8f43b7fdc47bc12dc927468111a5a4c3213829b
Matt Levy <milevy1@gmail.com>
2019-02-19 05:47:46 -0700

    Added seasonal summary

--- lib/team_statistics.rb
+++ lib/team_statistics.rb
@@ -128,4 +128,85 @@ module TeamStatistics
       end
 
   end
+
+  def seasonal_summary(team_id)
+    # Find all games by team_id
+    games_by_team_id = @games.find_all do |game|
+      game.away_team_id == team_id || game.home_team_id == team_id
+    end
+
+    # games Group_by season_id
+    games_grouped_by_season = games_by_team_id.group_by do |game|
+      game.season
+    end
+
+    # Group by pre-season / regular season
+    games_grouped_by_season_and_type = {}
+    games_grouped_by_season.each do |season_id, games|
+      games_grouped_by_season_and_type[season_id] = {}
+      if games_grouped_by_season_and_type[season_id][:preseason].nil?
+        games_grouped_by_season_and_type[season_id][:preseason] = {
+          wins: 0,
+          total_games: 0,
+          total_goals_scored: 0,
+          total_goals_against: 0 }
+      end
+      if games_grouped_by_season_and_type[season_id][:regular_season].nil?
+        games_grouped_by_season_and_type[season_id][:regular_season] = {
+          wins: 0,
+          total_games: 0,
+          total_goals_scored: 0,
+          total_goals_against: 0 }
+      end
+
+      games.each do |game|
+        if team_id == game.away_team_id
+          win = 1 if game.outcome.include?("away win")
+          win = 0 if game.outcome.include?("home win")
+          goals_scored = game.away_goals
+          goals_against = game.home_goals
+        else
+          win = 1 if game.outcome.include?("home win")
+          win = 0 if game.outcome.include?("away win")
+          goals_scored = game.home_goals
+          goals_against = game.away_goals
+        end
+
+        season_type = :preseason if game.type == "P"
+        season_type = :regular_season if game.type == "R"
+
+
+        games_grouped_by_season_and_type[season_id][season_type][:wins] += win
+        games_grouped_by_season_and_type[season_id][season_type][:total_games] += 1
+        games_grouped_by_season_and_type[season_id][season_type][:total_goals_scored] += goals_scored
+        games_grouped_by_season_and_type[season_id][season_type][:total_goals_against] += goals_against
+      end
+
+    end
+    
+    # Build expected hash from data above
+    result = {}
+    games_grouped_by_season_and_type.each do |season_id, season_type_hash|
+      result[season_id] = {}
+
+      season_type_hash.each do |season_type, stats|
+        result[season_id][season_type] = {}
+
+        if stats[:total_games] > 0
+          result[season_id][season_type][:win_percentage] = (stats[:wins] / stats[:total_games].to_f).round(2)
+          result[season_id][season_type][:average_goals_scored] = (stats[:total_goals_scored] / stats[:total_games].to_f).round(2)
+          result[season_id][season_type][:average_goals_against] = (stats[:total_goals_against] / stats[:total_games].to_f).round(2)
+        else
+          result[season_id][season_type][:win_percentage] = 0
+          result[season_id][season_type][:average_goals_scored] = 0
+          result[season_id][season_type][:average_goals_against] = 0
+        end
+
+        result[season_id][season_type][:total_goals_scored] = stats[:total_goals_scored]
+        result[season_id][season_type][:total_goals_against] = stats[:total_goals_against]
+      end
+    end
+    result
+  end
+
 end
