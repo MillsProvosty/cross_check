c25300714f8c8af0f8d643bcf83913383d64adb4
Mills Provosty <millsprovosty@gmail.com>
2019-02-21 07:53:24 -0700

    deleted some comments

--- lib/team.rb
+++ lib/team.rb
@@ -15,23 +15,4 @@ class Team
     @link = row[:link]
 
   end
-
-  # def game_by_teams(team_id)
-  #   game_array = []
-  #   @games.each do |game|
-  #     game_array << @games.team_id
-  #   end
-  # end
-
-  # def count_wins_by_team_id
-  #   win_count = game_array.each do |game|
-  #     game.wins
-  #   if @stat_tracker.wins == true
-  #     hash += 1
-  #   end
-  # end
-  # end
-
-
-
 end
